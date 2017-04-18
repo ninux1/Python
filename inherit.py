79bbf9f2b388ccc0795e6305d51c2abfa6f037bf
ninux1 <ninux.ad@gmail.com>
2017-04-17 22:09:58 -0700

    Inheritance

---
+++ inherit.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+class Parent:
+   parentAttr = 100
+   def __init__(self):
+     print "Calling parent constructor"
+   
+   def parentMethod(self):
+     print "Calling method parent"
+
+   def setAttr(self, attr):
+     Parent.parentAttr = attr
+
+   def getAttr(self):
+     print "Parent Attr :", Parent.parentAttr
+
+class Child(Parent):
+   def __init__(self):
+     print "Calling child constructor"
+
+   def childMethod(self):
+     print "Calling Child method"
+
+c = Child()
+c.childMethod()
+c.parentMethod()
+c.setAttr(200)
+c.getAttr()
