f046e015c95d6510312c7577c555b3cb31c7ad02
ninux1 <ninux.ad@gmail.com>
2017-07-30 20:13:07 -0700

    kwargs

--- args/kwargs.py
+++ args/kwargs.py
@@ -9,9 +9,13 @@ def kfunction(*args):
    for arg in args:
       print arg
 
-def kwarg(x=1, y=2):
-   print x ,y 
-   
+#def kwarg(x=1, y=2):
+def kwarg(*args, **kwargs):
+   #print x ,y 
+   for arg in args:
+       print arg
+   for item in kwargs.items():
+       print item	
 
 l = [1,2,3,4,5]
 m = [6,7,8,9,10]
@@ -19,5 +23,6 @@ m = [6,7,8,9,10]
 #function(1,2,3,4,5,'hans','new')
 #function(*l)
 #kfunction(*m)
-kwarg()
-#kwarg(x=12, y=14)
+#kwarg()
+kwarg(1,2,3,4,5,6,x=7, y=8)
+#kfunction(1,2,3,4,5,6,x=7, y=8)
