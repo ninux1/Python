4f3608f559406b262f46d14e75fc7ca93a71e9de
ninux <ninux.ad@gmail.com>
2017-08-20 23:31:32 -0700

    list directories recursively using listdir recursively

---
+++ lsdir.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+""" This python program takes in the path and prints all the contents from subdirectories as well"""
+
+import os
+
+def listdir(path):
+   if(os.path.isdir(path)):
+        for file in os.listdir(path):
+            filepath = os.path.join(path, file)
+            if os.path.isdir(filepath):
+                listdir(filepath)
+            else:
+                print(filepath)
+   else:
+       print("Plz enter a path to a directory")
+
+listdir('/home/ninux/temp')
+
+
