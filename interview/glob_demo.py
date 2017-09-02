b079c8364966cd846a60cd8d692261b7f09a0a28
Ninux Ad <ninux.ad@gmail.com>
2017-09-02 10:12:33 -0700

    python glob module for directory traverse

--- interview/glob_demo.py
+++ interview/glob_demo.py
@@ -2,7 +2,7 @@
 
 import glob
 
-""" glob.glob gives us all the contents of the particular directory and does 
+""" glob.glob gives us all the contents of the particular directory with wild card * and does 
     not recurse in the sub directories."""
 
 for name in glob.glob('C:\Pycharm\Python\*'):
