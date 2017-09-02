368536246d167cbd940fb562d22d9e142fa2948a
Ninux Ad <ninux.ad@gmail.com>
2017-09-02 10:11:53 -0700

    import python module concept

--- if_main/first.py
+++ if_main/first.py
@@ -1,10 +1,19 @@
-
 #!/usr/bin/env python
 
-def main():
-    print("In Main function")
 
-if __name__ == '__main__':
-   print("direct Call")
-else:
-   print("Imported") 
+class Display:
+    def __init__(self, name):
+        self.a = 20
+        self.b = 30
+        print("Name = {}".format(name))
+
+    def run(self):
+        print("The Sum is {}".format(self.a+self.b))
+
+#app = Display()
+
+#if __name__ == '__main__':
+#    print("Direct Call")
+#    app.run()
+#else:
+#    print("From imported module")
--- if_main/second.py
+++ if_main/second.py
@@ -1,5 +1,21 @@
 
 #!/usr/bin/env python
 
-import first
-print(__name__)
+#import first
+#print(__name__)
+
+from first import Display
+
+app = Display(__name__)
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+
+
+
+
+
+
