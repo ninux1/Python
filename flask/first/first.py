30df453c64143e48a81ec854241b3326a3d41101
Ninux Ad <ninux.ad@gmail.com>
2017-09-02 17:48:09 -0700

    Python Flask modules

--- flask/first/first.py
+++ flask/first/first.py
@@ -1,1 +1,12 @@
+#!/usr/bin/env python
 
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def task_structure():
+    return "Task Server works here"
+
+if __name__ == '__main__':
+    app.run(port=5005)
