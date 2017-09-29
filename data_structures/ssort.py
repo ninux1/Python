7c9190e17e1a4935dc33e361a5df52bd819b01d1
Ninux Ad <ninux.ad@gmail.com>
2017-09-29 15:09:13 -0700

    linkedlist

---
+++ data_structures/ssort.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+
+a = [20, 30, 15, 7, 1, -3, -4, 6, 100, -1, 21, 200, 0, 16]
+
+
+for i in range(len(a) - 1):
+    index = i
+    for j in range(i+1, len(a)):
+        if a[j] < a[index]:
+            index = j
+
+    if index is not i:
+        temp = a[index]
+
+        a[index] = a[i]
+        a[i] = temp
+
+print("The sorted is {} ". format(a))
+
+
+
