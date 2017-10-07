4a38494e701f58287c10c979d00307c0dfad90b5
Ninux Ad <ninux.ad@gmail.com>
2017-10-06 19:28:26 -0700

    Heap deletion of node

--- data_structures/heap.py
+++ data_structures/heap.py
@@ -26,8 +26,38 @@ class Heap:
             pidx = (pidx - 1) // 2
 
     def heap_pop(self):
+        parentidx = 0
         popped = self.heap[0]
+        self.heap[0] = self.heap[len(self.heap) - 1]
+        del self.heap[len(self.heap) - 1]
+        self.reconstruct_heap(parentidx)
         return popped
+
+    def reconstruct_heap(self, parentidx):
+
+        leftidx = 2*parentidx + 1
+        rightidx = 2*parentidx + 2
+
+        if leftidx == len(self.heap):
+            return
+        if rightidx == len(self.heap):
+            if self.heap[parentidx][1] < self.heap[leftidx][1]:
+                self.swap(parentidx, leftidx)
+                parentidx = (2 * parentidx) + 1
+            else:
+                return
+        else:
+            if self.heap[parentidx][1] < max(self.heap[leftidx][1], self.heap[rightidx][1]):
+                if self.heap[leftidx] == max(self.heap[leftidx], self.heap[rightidx]):
+                    self.swap(parentidx, leftidx)
+                    parentidx = (2*parentidx) + 1
+                if self.heap[rightidx] == max(self.heap[leftidx], self.heap[rightidx]):
+                    self.swap(parentidx, rightidx)
+                    parentidx = (2*parentidx) + 2
+            else:
+                return
+
+        self.reconstruct_heap(parentidx)
 
     def swap(self, pidx, cidx):
         temp = self.heap[pidx]
@@ -50,5 +80,8 @@ if __name__ == '__main__':
     h.heap_push(("Node6", 8))
     h.heap_push(("Node5", 6))
     h.heap_push(("Node10", 16))
+
     print(h.heap_pop())
+    print(h.heap_pop())
+
     print(h.heap)
